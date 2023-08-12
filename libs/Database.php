3b8e6b989654729d19a2c49c14cf5cfdf7a1eccb
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2023-08-12 17:11:27 +0700

    Update Database.php

--- libs/Database.php
+++ libs/Database.php
@@ -4,8 +4,8 @@ class Database extends PDO
     public function __construct()
     {
         $dsn = "mysql:host=localhost;dbname=rspgkppp_db;charset=utf8mb4";
-        // parent::__construct($dsn, 'root', '');
-        parent::__construct($dsn, 'rspgkppp_jirasak', 'Merlin5409.');
+        parent::__construct($dsn, 'root', '');
+        // parent::__construct($dsn, 'rspgkppp_jirasak', 'Merlin5409.');
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 }
