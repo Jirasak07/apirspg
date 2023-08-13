67488c210c23b473c931c8ca8359d4fb3c298a82
Jirasak07 <bee.110243@gmail.com>
2023-08-13 10:17:43 +0700

    bb

---
+++ controllers/User.php
@@ -0,0 +1,24 @@
+<?php
+	class User extends Controller {
+	    function __construct(){
+	    		parent::__construct();
+			}
+			
+	    function index(){
+			header("Content-Type: application/json; charset=UTF-8");
+				$arr = array(
+					'status' => 200,
+					'status_name' => header_status(200),
+					'data' => array(
+						'success' => "ไม่อนุญาตให้เข้าใช้งานส่วนใดของระบบ"
+					)
+				);
+				echo json_encode($arr);
+				http_response_code(200);
+	    }
+       function AddUser(){
+        $this->model->AddUser();
+       }
+
+	}
+?>
---
+++ models/User_model.php
@@ -0,0 +1,42 @@
+<?php
+ 	header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
+	class User_model extends Model{
+		function __construct(){
+				parent::__construct();
+		}
+		function AddUser(){
+            $json = json_decode(file_get_contents("php://input"));
+            $password = $json->password;
+            $name = $json->name;
+            $username = $json->username;
+            $organize = $json->organize;
+            $tell = $json->tell;
+            $citizen = $json->citizen;
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+            $sql = $this->db->prepare("
+            SELECT COUNT(*) AS total FROM tb_user
+            ");
+            $sql->execute(array());
+            $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+            $id = 0;
+            $data = intval($data[0]['total']);
+            if($data <= 0 ){
+                $id = 1;
+            }else{
+                $id = $data +1;
+            }
+            $sqlAdd = $this->db->prepare("
+            INSERT INTO tb_user VALUES('$id','$username','$hashed_password','$name','$organize','2','$tell','$citizen','1')
+            ");
+            if($sqlAdd->execute()){
+                echo json_encode("success",JSON_PRETTY_PRINT);
+            }else{
+                echo json_encode("error",JSON_PRETTY_PRINT);
+            }
+            // $token =  GenarateToken($data[0]['username']);
+            // echo json_encode($token,JSON_PRETTY_PRINT);
+        }	
+}            
+                     
+?>
