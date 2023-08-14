51f5ac64cc2e93fd7405ba846cf7a752bf2c61bf
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2023-08-14 13:30:18 +0700

    Update PlantPD.php

--- PlantPD.php
+++ PlantPD.php
@@ -1,6 +1,9 @@
 <?php 
 require_once __DIR__.'/vendor/autoload.php';
-$mpdf = new \Mpdf\Mpdf();
+$mpdf = new \Mpdf\Mpdf([
+    'default_font_size'=>16,
+    'default_font'=>'sarabun'
+]);
 $mpdf->WriteHTML('Hello World สวัสดีครับ');
 
 $mpdf->Output();
