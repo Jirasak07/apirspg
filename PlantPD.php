69627b74af1d0c95e298650d31510e06d84dc9ee
Jirasak07 <bee.110243@gmail.com>
2023-08-14 15:33:04 +0700

    Update PlantPD.php

--- PlantPD.php
+++ PlantPD.php
@@ -4,14 +4,7 @@ $mpdf = new \Mpdf\Mpdf([
     'default_font_size'=>16,
     'default_font'=>'sarabun'
 ]);
-<<<<<<< Updated upstream
-$mpdf->SetImportUse(); // only with mPDF <8.0
+$mpdf->SetImportUse(true); // only with mPDF <8.0
 $mpdf->SetDocTemplate('/as-2.pdf',true);
-=======
-$html = '<div style="text-align:center;" >Hello World สวัสดีครับ</div>';
-$mpdf->SetImportUse();
-$mpdf->SetDocTemplate('as-2.pdf',true);
-$mpdf->WriteHTML($html);
->>>>>>> Stashed changes
 $mpdf->Output();
 ?>
