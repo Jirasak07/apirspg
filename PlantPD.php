a127750b9c2d6a7a997934dba63d21ec115cd295
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2023-08-14 12:56:33 +0700

    hh

---
+++ PlantPD.php
@@ -0,0 +1,13 @@
+<?php 
+require_once __DIR__.'/vendor/autoload.php';
+// require dirname(__FILE__) ;
+// require('vendor/autoload.php');
+// Create an instance of the class:
+$mpdf = new \Mpdf\Mpdf();
+
+// Write some HTML code:
+$mpdf->WriteHTML('Hello World');
+
+// Output a PDF file directly to the browser
+$mpdf->Output();
+?>
--- PlantPDF/PlantPD.php
+++
@@ -1,6 +0,0 @@
-<?php 
-require_once __DIR__.'../vendor/autoload.php';
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<div>สวัสดี Hellow</div>');
-$mpdf->Output();
-?>
