22757ca177cdb87ab323a54faac698841e32dd19
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2024-10-01 11:07:18 +0700

    Create Database.php

---
+++ models/Database.php
@@ -0,0 +1,13 @@
+<?php
+class Database extends PDO
+{
+    public function __construct()
+    {
+
+        $dsn = "mysql:host=localhost;dbname=rspgkppp_dbnew;charset=utf8mb4";
+        parent::__construct($dsn, 'rspgkppp_sa', 'Merlin5525.');
+        // parent::__construct($dsn, 'root', '');
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+}
+        // $dsn = "mysql:host=https://s054ns1.hostinghispeed.com/;dbname=rspgkppp_dbnew;charset=utf8mb4";
